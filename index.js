3ff5269f6f48e553094f25205027fb1ce6f6aded
OmingoEmma <emmaomingo@gmail.com>
2024-04-04 08:13:59 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const cards = ["teddy", "dron", "dolly"];
+
+function writeCards(cards) {
+  const messages = [];
+  for (let i = 0; i < cards.length; i++) {
+    messages.push(`Thank you, ${cards[i]} for the wonderful surprise gift!`);
+  }
+  return messages;
+}
+
+console.log(writeCards());
+
+const n = 11;
+function countDown(n) {
+  let i = n;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+
+console.log(countDown(n));
